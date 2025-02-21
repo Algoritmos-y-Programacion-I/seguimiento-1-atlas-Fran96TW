5e347bdf1337094d2bdf395130dac63a27cdb691
Fran96TW <francisconauffalb@hotmail.com>
2025-02-21 13:06:08 -0500

    seg1

--- src/ui/Atlas.java
+++ src/ui/Atlas.java
@@ -1,64 +1,102 @@
-/*
-* Analisis
-* Descripcion del programa:
-* Entradas:
-* Salidas: 
-* Ejemplo:
-*/
-
 package ui;
 
 import java.util.Scanner;
 
 public class Atlas {
 
-	private Scanner escaner;
-
-    // Declaracion de constantes (completar)
-    final double COSTO_TRAYECTO_AVION = 250000;
-
-	private Atlas() {
-		escaner = new Scanner(System.in);
-	}
-	
-	
-	public void run()
-	{   
-        // Declaracion de info a pedir del usuario (completar)
-        String nombre;
-        // Saludo
-        System.out.println("Bienvenido al programa de viajes EVENTOS ATLAS realizado por los estudiantes de Algoritmos y programacion I de la Universidad ICESI.");
-        // Pedir info al usuario (completar)
-        System.out.println("Cual es tu nombre?");
-        nombre = escaner.nextLine();
-
-        // ...
-        System.out.println("Bienvenido " + nombre + "!");
-        // ...
-
-        // Declaracion de salidas calculadas (completar)
-        double totalTransporte;
-        // Calculo de salidas mediante metodos (completar)
-        totalTransporte = calcularTotalTransporte(); // aqui podrian faltar parametros
-	}
-
-	public static void main(String[] args) {
-		Atlas mainApp = new Atlas();
-		mainApp.run();
-	}
-
-    // Completar metodos para calcular salidas
-
-    /**
-	 * Descripción: El método ... permite calcular ...
-	 * @param 
-	 * @return 
-	 */
-    public double calcularTotalTransporte() {
-        return 0; // Completar operacion
+    public static void main(String[] args) {
+        personalInfo();
     }
 
-	
+    public static void personalInfo(){
 
-	
+        String name;
+        int Age;
+        int Identification;
+        String City;
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Hello User, ¿Can you please identify your self?");
+        System.out.print("Name: ");
+        name = scanner.nextLine();
+        System.out.print("Age: ");
+        Age = scanner.nextInt();
+        System.out.print("Identification: ");
+        Identification = scanner.nextInt();
+        System.out.print("¿To what city you are going to?: ");
+        City = scanner.nextLine();
+        
+        scanner.nextLine();
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + Age);
+        System.out.println("DNI: " + Identification);
+        System.out.println("City:" + City);
+        System.out.print("We want to certify that your information is correct, it is? ");
+        String tc = scanner.nextLine();
+
+        if (tc.equalsIgnoreCase("Yes")) {
+            nights(scanner, name);
+        } else if (tc.equalsIgnoreCase("No") ){
+            System.out.println("Can you pls put your info correct");
+            personalInfo();
+        }
+
+        scanner.close(); // Close the Scanner
+    }
+
+    public static void nights(Scanner scanner, String name){
+        int night;
+
+        System.out.print(name + " ¿How many nights you are gonna to stay?(Remember that you have to stay minimum one night): ");
+        night = scanner.nextInt();
+
+        if (night < 1){
+            System.out.println("You can´t stay less than one night");
+            nights(scanner, name);
+        }else if (night <= 4){
+
+        }else{
+            System.out.println("You can´t be more than 4 nights");
+            nights(scanner, name);
+        }
+
+
+    }
+
+    public static int nightCalc(int night) {
+        int nightsCost = night*150000;
+
+        return nightsCost;
+    }
+
+    public static int planeCost(){
+        int costPlane = 500000;
+
+        return costPlane;
+    }
+
+    public static int total(int costPlane, int nightsCost){
+        int totalp = costPlane + nightsCost;
+
+        return totalp;
+    }
+
+    public static void displayInvoice(int night, int totalp) {
+        // Display invoice decorated in Eventos Atlas theme
+        System.out.println("\n*******************************************\n" +
+                "*** Eventos Atlas - Factura de Venta ***\n" +
+                "*******************************************\n" +
+                "Noches vendidas ...................... " + night + "\n" +
+                "Total Alojamiento .................... $" + nightCalc(night) + "\n" +
+                "Total Transporte .................... $" + planeCost() + "\n" +
+                "*******************************************\n" +
+                "Total a Pagar ....................... $" + totalp + "\n" +
+                "*******************************************\n" +
+                "¡Gracias por preferirnos! Eventos Atlas\n" +
+                "*******************************************\n");
+    }
+
+
+
 }
